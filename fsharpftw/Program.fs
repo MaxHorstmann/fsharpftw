7a2aff11533304044eec7ef0f56550abf1ac03bf
Max Horstmann <mhorstmann@stackoverflow.com>
2015-09-11 15:45:36 -0400

    generics

--- fsharpftw/Program.fs
+++ fsharpftw/Program.fs
@@ -5,6 +5,18 @@ let main argv =
     [1..100] 
       |> List.map square 
       |> printfn "%A"
+
+    let foo = 123
+    let fooasbyte = byte foo
+
+    let genericfunction x = 
+       printfn "%A" x
+
+
+
+
+
+
     
     
     0 // return an integer exit code
